f026e612ae580faecdf100a4ecabcd6b4aedbc65
cole peden <cole.peden@outlook.com>
2025-02-20 01:53:59 -0500

    diagnostic bind + change term exit bind

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-vim.cmd('source ~/.config/nvim/vimbindings.vim')
+vim.cmd('source ~/.config/nvim/vimbindings.vim') -- escape hatch
 
 vim.g.markdown_fenced_languages = {
 	"ts=typescript"
@@ -8,3 +8,13 @@ vim.g.markdown_fenced_languages = {
 -- require'lspconfig'.svelte.setup{}
 require'lspconfig'.eslint.setup{}
 require'lspconfig'.ts_ls.setup{}
+
+vim.g.mapleader = '\\'
+
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+vim.keymap.set('n', '<leader>d', function() 
+					vim.diagnostic.open_float()
+				end)
+
+
