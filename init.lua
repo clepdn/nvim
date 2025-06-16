2f4308da39129852cd3d0d3985c2bd0b254ad039
cole peden <cole.peden@outlook.com>
2025-06-15 20:06:03 -0400

    sync

--- init.lua
+++ init.lua
@@ -1,21 +1,28 @@
 vim.g.mapleader = '\\'
 
+vim.opt.clipboard:append("unnamedplus")
+
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 vim.keymap.set('n', '<leader>d', function() 
 					vim.diagnostic.open_float()
 				end)
 
+-- ai slop bind - verify if ture
+vim.keymap.set('n', 'gy', ':tabp<CR>', { noremap = true, silent = true })
+
 require('config.lazy')
 
 vim.cmd('source ~/.config/nvim/vimbindings.vim') -- escape hatch
 
 vim.g.markdown_fenced_languages = {
-	"ts=typescript"
+	"ts=typescript",
+	"tsx=typescript"
 }
 
-require'lspconfig'.eslint.setup{}
+-- require'lspconfig'.eslint.setup{}
 require'lspconfig'.ts_ls.setup{}
+require'lspconfig'.clangd.setup{}
 
 --[=====[
 --]=====]
