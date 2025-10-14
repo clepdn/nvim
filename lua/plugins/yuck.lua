e5b3a790a5bd9b89c881a6884ef6e544f1d93540
Callie Peden <calliepeden@gmail.com>
2025-10-14 18:05:07 -0400

    yuh

---
+++ lua/plugins/yuck.lua
@@ -0,0 +1,12 @@
+return {
+    "elkowar/yuck.vim",
+    event = "VeryLazy",
+    dependencies = {},
+    opts = {},
+    cmd = {},
+    keys = {},
+    url = "https://github.com/elkowar/yuck.vim.git",
+    config = function()
+    end,
+    ft = "yuck",
+}
